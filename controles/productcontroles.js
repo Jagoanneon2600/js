715613d57b750353bd887b66bfbc8c25b3ed2d2b
Jagoanneon2600 <124511005+Jagoanneon2600@users.noreply.github.com>
2023-05-09 14:11:06 +0700

    js

---
+++ controles/productcontroles.js
@@ -0,0 +1,9 @@
+import Product from "../model/ProductModel";
+import path from 'path';
+import fs from 'fs';
+
+export const getProduct = async (req,res)=>{};
+export const getProductById = async(req,res)=>{};
+export const crearteProduct = async(req,res)=>{};
+export const updateProduct = async(req,res)=>{};
+export const deleteProduct = async(req,res)=>{}
